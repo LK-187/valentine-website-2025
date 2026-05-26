1211794608a55ae4e948df4647653eb3f3b1775c
LK-187 <ali2008salam1@gmail.com>
2026-05-26 05:23:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ window.VALENTINE_CONFIG = {
     valentineName: "اريج",
     pageTitle: "كل عام وانتِ عيدي يا اريج",
     floatingEmojis: {
-    
+        hearts: ["❤️", "💖", "💝"],
+        bears: ["🧸"]
     },
     questions: {
         first: {
@@ -23,12 +24,12 @@ window.VALENTINE_CONFIG = {
         }
     },
     loveMessages: {
-        extreme: "واوووو كل هذا غلا؟؟",
-        high: "إلى اللانهاية وما بعدها!",
-        normal: "واكثر بعد بمليون مرة!"
+        extreme: "الله واني اموت عليج هم 🙈",
+        high: "الله واني اموت عليج هم 🙈",
+        normal: "الله واني اموت عليج هم 🙈"
     },
     celebration: {
-        title: "الله واني اموت عليج هم 🙈",
+        title: "يا احلى اعيادي واسعد ايام عمري.. 🎉",
         message: "كل عيد وانتِ وياي، ربي يخليج الي ولا يحرمني من عيونج.. اعشقج واموت عليج يا بعد بيتي",
         emojis: ""
     },
